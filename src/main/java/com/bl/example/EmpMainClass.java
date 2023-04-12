e969d06e4ef9e906190815ed3e1287d1da07d116
ymr00 <ymr251318@gmail.com>
2023-04-12 09:09:41 +0530

    Adding EmployeeWage UC1 to branch

--- src/main/java/com/bl/example/EmpMainClass.java
+++ src/main/java/com/bl/example/EmpMainClass.java
@@ -3,5 +3,12 @@ package com.bl.example;
 public class EmpMainClass {
     public static void main(String[] args) {
         System.out.println(" Welcome to the Master Branch.");
+
+        int a = ( int )( Math.random() * 10 ) % 2;
+
+        if( a == 1 )
+            System.out.println( "Employee is Present.");
+        else
+            System.out.println( "Employee is Absent.");
     }
 }
